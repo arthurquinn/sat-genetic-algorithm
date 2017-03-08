8773cf1220d2d3034fe8eae99aeda0df8215d60f
Arthur Quinn <arthurquinn92@gmail.com>
2017-03-08 18:36:16 -0500

    fixed bug where chromosomes were not cloned

--- src/app/Chromosome.java
+++ src/app/Chromosome.java
@@ -6,121 +6,87 @@ import java.util.Random;
 
 public class Chromosome {
 		
-	private boolean[] varAssignments;
+	private boolean[] values;
 
-	public Chromosome(int numVars) {
-		this.varAssignments = new boolean[numVars];
-		
-		Random random = new Random();
+	public Chromosome(int numVars, Random random) {
+		this.values = new boolean[numVars];
 		for (int i = 0; i < numVars; i++) { 
 			if (random.nextFloat() >= 0.50)
-				this.varAssignments[i] = true;
+				this.values[i] = true;
 		}
 	}
 	
-	public boolean[] getVarAssignments() { return this.varAssignments; }
-	
-	public boolean getAssignment(int idx) {
-		return varAssignments[idx];
-	}
-	
-	public void setAssignment(boolean val, int idx) {
-		varAssignments[idx] = val;
-	}
-	
-	
-	// Performs the flip heuristic operation
-	public void flipHeuristic(int evalVal, ClauseList clauseList) {
-		Random random = new Random();
-		List<Integer> idxToFlip = new ArrayList<Integer>();
-		int gain = 1;
-		
-		int flipIters = 0;
-		
-		// Continue the flip heuristic as long as the flips yielded a net gain in the evaluation function values
-		while (gain > 0) {
-			int tempVal = -1;
-			int startVal = evalVal;
-			
-			System.out.println("Performing " + flipIters++ + "th flip iteration");
-			
-			// Maintain a list of assignment indexes we have yet to flip
-			for (int i = 0; i < varAssignments.length; i++) {
-				idxToFlip.add(i);
-			}
-			for (int i = 0; i < varAssignments.length; i++) {
-				
-				// Get a random assignment index and remove it from the list
-				int idxIdx = random.nextInt(idxToFlip.size());
-				int flipIdx = idxToFlip.remove(idxIdx);
-				
-				// Flip the assignment and calculate the evaluation value after the flip
-				boolean origVal = getAssignment(flipIdx);
-				setAssignment(!origVal, flipIdx);
-				tempVal = performEvaluationFunction(clauseList);
-				
-				// If the flip results in an improvement, set the new evaluation value and keep the flip; else discard the flip
-				if (tempVal >= evalVal)
-					evalVal = tempVal;
-				else
-					setAssignment(origVal, flipIdx);
-			}
-			gain = evalVal - startVal;
+	public Chromosome(Chromosome c) {
+		values = new boolean[c.getValues().length];
+		for (int i = 0; i < c.getValues().length; i++) {
+			this.values[i] = c.getValues()[i];
 		}
 	}
 	
+	public boolean[] getValues() { return this.values; }
 	
-	// Mutates the chromosome; Here mutation is defined as flipping each var assignment with probability 0.50
-	public void mutate() {
-		Random random = new Random();
-		for (int i = 0; i < varAssignments.length; i++) {
-			if (random.nextDouble() < 0.50) {
-				varAssignments[i] = !varAssignments[i];
-			}
-		}
-	}
-	
-	// Performs a uniform crossover with another chromosome; numVars must be consistent for each chromosome
-	public void uniformCrossover(Chromosome c) {
-		Random random = new Random();
-		for (int i = 0; i < varAssignments.length; i++) {
-			// Assign theirs to ours with 0.50 prob; Assign ours to theirs with 0.50 prob
-			if (random.nextDouble() < 0.50)
-				setAssignment(c.getAssignment(i), i);
-			else
-				c.setAssignment(getAssignment(i), i);
-		}
-	}
-	
-	
-	// Performs the evaluation function; here the evaluation function is the number of clauses satisfied by this chromosome
-	public int performEvaluationFunction(ClauseList clauseList) {
-		
-		int n = 0;
-		List<Integer[]> cl = clauseList.getClauses();
-		
-		// Apply each chromosome to the clause list and determine the number of clauses satisfied
-		for (int i = 0; i < cl.size(); i++) {
-			n = applyChromosomeToClause(cl.get(i)) ? n + 1 : n;
-		}
-		
-		return n;
-	}
-	
-	
-	// Determines if this chromosome satisfies a clause
-	private boolean applyChromosomeToClause(Integer[] clause) {
-		
-		// Determine if the assignments of this chromosome satisfy the clause
-		for (int i = 0; i < clause.length; i++) {
-			
-			// See if the assignment is true and not it if necessary; if the assignment is true the entire clause is true
-			boolean a = varAssignments[Math.abs(clause[i]) - 1];
-			if (clause[i] < 0)
-				a = !a;
-			if (a)
-				return a;
-		}
-		return false;
-	}
+    public int calcEvaluationValue(ClauseList clauseList) {
+        int f = 0;
+        List<Integer[]> cl = clauseList.getClauses();
+        for (int i = 0; i < cl.size(); i++) {
+            for (int j = 0; j < 3; j++) { 
+                boolean val = values[Math.abs(cl.get(i)[j]) - 1];
+                if (cl.get(i)[j] < 0)
+                    val = !val;
+                if (val) {
+                    f += 1;
+                    break;
+                }
+            }
+        }
+        return f;
+    }
+
+    public void uniformCrossover(Chromosome c, Random random) {
+        for (int i = 0; i < values.length; i++) {
+            // They get our value with 0.50 prob; we get theirs with 0.50 prob
+            if (random.nextDouble() < 0.50f)
+                c.values[i] = values[i];
+            else
+                values[i] = c.values[i];
+        }
+    }
+
+    public void mutate(Random random) {
+        for (int i = 0; i < values.length; i++)
+            if (random.nextDouble() < 0.50)
+                values[i] = !values[i];
+    }
+
+    public void flipHeuristic(int initEval, ClauseList clauseList, Random random) {
+        int gain = 1;
+        int startEval = initEval;
+        int currEval = initEval;
+
+        while (gain > 0) {
+            startEval = currEval;
+            List<Integer> remainIdx = new ArrayList<Integer>();
+            for (int i = 0; i < values.length; i++) {
+                remainIdx.add(i);
+            }
+            
+            for (int i = 0; i < values.length; i++) {
+                // Get random index
+                int idx = remainIdx.remove(random.nextInt(remainIdx.size()));
+
+                // Flip value at idx
+                values[idx] = !values[idx];
+                int eval = calcEvaluationValue(clauseList);
+
+                // If flip netted us a gain, update current eval and accept flip; else reject flip
+                if (eval >= currEval)
+                    currEval = eval;
+                else
+                    values[idx] = !values[idx];
+            }
+
+            // Check if all flips netted us a gain
+            gain = currEval - startEval; 
+        }
+    }
 }
--- src/app/SATGeneticAlgorithm.java
+++ src/app/SATGeneticAlgorithm.java
@@ -10,183 +10,132 @@ public class SATGeneticAlgorithm {
 	
 	private Chromosome[] population;
 	
+	private Random random;
+	
 	public SATGeneticAlgorithm(ClauseList clauseList, int populationSize) {
 		this.clauseList = clauseList;
+		this.random = new Random();
 		this.population = new Chromosome[populationSize];
 		for (int i = 0; i < populationSize; i++) {
-			this.population[i] = new Chromosome(clauseList.getNumVars());
+			this.population[i] = new Chromosome(clauseList.getNumVars(), random);
 		}
 	}
 	
-	public void runAlgorithm() {
-		
-		int iters = 0;
-		boolean improved = true;
-		int evalSum = 0;
-		int[] evalValues = new int[population.length];
-		
-		while (improved) {
-			// Step 0: Perform the evaluation function for each chromosome in the population
-			
-			System.out.println("BEGIN ITERATION: " + iters);
-			
-			if (iters == 0) {
-				for (int i = 0; i < population.length; i++) {
-					evalValues[i] = population[i].performEvaluationFunction(clauseList);
-					evalSum += evalValues[i];
-					System.out.println(evalValues[i]);
-				}
-			}
-			// int[] evalValues = Arrays.stream(population).mapToInt(c -> c.performEvaluationFunction(clauseList)).toArray();
-			
-			
-			// Step 1: Get two elite chromosomes from the population
-			int eliteIdxs[] = evaluateElite(evalValues);
-			
-			System.out.println(eliteIdxs[0]);
-			System.out.println(eliteIdxs[1]);
-			
-			
-			
-			
-			// Step 2: Probabalistically select the most fit chromosomes in the population for reproduction (elite automatically pass to step 3)
-			
-			// double[] evalProb = Arrays.stream(evalValues).mapToDouble(i -> (double)i / (double)evalSum).toArray();
-			
-			// evaluate their probabilities
-			double[] evalProbUpperBounds = new double[population.length];
-			evalProbUpperBounds[0] = (double)evalValues[0] / (double)evalSum;
-			System.out.println(evalProbUpperBounds[0]);
-			for (int i = 1; i < population.length; i++) {
-				evalProbUpperBounds[i] = evalProbUpperBounds[i - 1] + ((double)evalValues[i] / (double)evalSum);
-				System.out.println(evalProbUpperBounds[i]);
-			}
-			evalProbUpperBounds[population.length - 1] = 1.0; // set the last upper bound to be 1 to resolve precision issues
-	
-			
-			// select based on their fitness (probability of selection based on evaluation function value) (elite are guaranteed selection)
-			population = performProbabilisticSelection(evalProbUpperBounds, eliteIdxs);
-			
-			
-			// Step 3: Uniformly crossover two adjacent chromosomes (ignore elite - they continue intact)
-			performUniformCrossover(eliteIdxs);
-			
-			// Step 4: Perform disruptive mutation (ignore elite - they continue intact)
-			performDisruptiveMutation(eliteIdxs);
-			
-			// Step 5: Flip heuristic
-			performFlipHeuristic(eliteIdxs, evalValues);
-			
-			
-			// Calculate new evaluation values after this iteration completes
-			improved = false;
-			evalSum = 0;
-			for (int i = 0; i < population.length; i++) {
-				int temp = population[i].performEvaluationFunction(clauseList);
-				System.out.println("new eval function calculated: " + temp);
-				if (temp > evalValues[i])
-					improved = true;
-				if (temp == clauseList.getClauses().size()) {
-					System.out.println("Solution found!!!");
-					return;
-				}
-				evalValues[i] = temp;
-				evalSum += temp;
-			}
-			iters++;
-		}
-		
-		System.out.println("No improvements made... terminating");
-	}
-	
-	private void performFlipHeuristic(int[] eliteIdxs, int[] startEvalVals) {
-		for (int i = 0; i < population.length; i++) {
-			if (i != eliteIdxs[0] && i != eliteIdxs[1]) {
-				System.out.println("Beginning flip heuristic for " + i);
-				population[i].flipHeuristic(startEvalVals[i], clauseList);
-			}
-		}
-	}
-	
-	private void performDisruptiveMutation(int[] eliteIdxs) {
-		Random random = new Random();
-		for (int i = 0; i < population.length; i++) {
-			// A chromosome is mutated with probability 0.90
-			if (i != eliteIdxs[0] && i != eliteIdxs[1] && random.nextDouble() < 0.90) {
-				// Mutation here is defined as flipping each var assignment with probability 0.50
-				System.out.println("mutating " + i);
-				population[i].mutate();
-			}
-		}
-	}
-	
-	private void performUniformCrossover(int[] eliteIdxs) {
-		int xIdx = 0;
-		int yIdx = 0;
-		for (int i = 0; i < (population.length / 2) - 1; i++) {
-			while (xIdx == eliteIdxs[0] || xIdx == eliteIdxs[1]) {
-				xIdx++;
-			}
-			yIdx = xIdx + 1;
-			while (yIdx == eliteIdxs[0] || yIdx == eliteIdxs[1]) {
-				yIdx++;
-			}
-			System.out.println("performing crossover on " + xIdx + " and " + yIdx);
-			population[xIdx].uniformCrossover(population[yIdx]);
-			xIdx = yIdx + 1;
-		}
-	}
-	
-	
-	private Chromosome[] performProbabilisticSelection(double[] evalProbUpperBounds, int[] eliteIdxs) {
-		Random random = new Random();
-		Chromosome[] newPop = new Chromosome[population.length];
-		for (int i = 0; i < population.length; i++) {
-			if (i != eliteIdxs[0] && i != eliteIdxs[1]) {
-				double p = random.nextDouble();
-				for (int j = 0; j < population.length; j++) {
-					if (p < evalProbUpperBounds[j]) {
-						newPop[i] = population[j];
-						System.out.println("selecting " + j + " to replace " + i);
-						break;
-					}
-				}	
-			} else {
-				System.out.println("Preserving elite at " + i);
-				newPop[i] = population[i];
-			}
-		}
-		return newPop;
-	}
-	
-	
-	// Determine the two most elite evaluation values
-	private int[] evaluateElite(int[] evalVals) {
-		int[] idxs = new int[2];
-		
-		int max1 = 0;
-		int max2 = 0;
-		
-		
-		for (int i = 0; i < evalVals.length; i++) {
-			if (evalVals[i] > max1) {
-				
-				// swap max1 to max2
-				max2 = max1;
-				idxs[1] = idxs[0];
-				
-				// set new max 1
-				max1 = evalVals[i];
-				idxs[0] = i;
-				
-			} else if (evalVals[i] > max2) { 
-				
-				// set new max 2
-				max2 = evalVals[i];
-				idxs[1] = i;
-			}
-		}
-		
-		return idxs;
-	}
+    public boolean runAlgorithm() {
+    	
+    	long start = System.currentTimeMillis();
+    	
+        // Calculate eval values
+        int evalSum = 0;
+        int[] eval = new int[population.length];
+        for (int i = 0; i < population.length; i++) {
+            eval[i] = population[i].calcEvaluationValue(clauseList);
+            evalSum += eval[i];
+            System.out.println("" + i + ": " + eval[i]);
+        }
+
+        // Calculate selection probs (upper bounds)
+        double[] probUpper = new double[population.length];
+        double acc = 0;
+        for (int i = 0; i < population.length; i++) {
+            acc += (double)eval[i] / (double)evalSum;
+            probUpper[i] = acc;
+            System.out.println("" + i + ": " + probUpper[i]);
+        }
+        probUpper[population.length - 1] = 1.0f; // set to 1 to resolve precision errors
+
+
+        // Determine elite
+        int[] eliteIdx = new int[2];
+        int max1 = 0;
+        int max2 = 0;
+        for (int i = 0; i < population.length; i++) {
+            if (eval[i] > max1) {
+                // swap max1 to max2
+                max2 = max1;
+                eliteIdx[1] = eliteIdx[0];
+
+                // set new max 1
+                max1 = eval[i];
+                eliteIdx[0] = i;
+            }
+            else if (eval[i] > max2) {
+                // set new max 2
+                max2 = eval[i];
+                eliteIdx[1] = i;
+            }
+        }
+
+        System.out.println("elite1: " + eliteIdx[0]);
+        System.out.println("elite2: " + eliteIdx[1]);
+
+        // Probabilistic selection
+        Chromosome[] newPop = new Chromosome[population.length];
+        for (int i = 0; i < population.length; i++) {
+            if (i != eliteIdx[0] && i != eliteIdx[1]) {
+                double p = random.nextDouble();
+                for (int j = 0; j < population.length; j++) {
+                    if (p < probUpper[j]) {
+                        newPop[i] = new Chromosome(population[j]);
+                        System.out.println("selecting " + j + " to replace " + i);
+                        break;
+                    }
+                }
+            } else {
+            	System.out.println("Preserving elite at " + i);
+                newPop[i] = population[i];
+            }
+        }
+        population = newPop;
+
+        // Uniform crossover
+        int xIdx = 0;
+        int yIdx = 0;
+        for (int i = 0; i < (population.length / 2) - 1; i++) {
+            while (xIdx == eliteIdx[0] || xIdx == eliteIdx[1]) {
+                xIdx++;
+            }
+            yIdx = xIdx + 1;
+            while (yIdx == eliteIdx[0] || yIdx == eliteIdx[1]) {
+                yIdx++;
+            }
+            System.out.println("performing crossover on " + xIdx + " and " + yIdx);
+            population[xIdx].uniformCrossover(population[yIdx], random);
+            xIdx = yIdx + 1;
+        }
+
+
+        // Disruptive mutation
+        for (int i = 0; i < population.length; i++) {
+            // mutate non-elite with prob 0.90
+            if (i != eliteIdx[0] && i != eliteIdx[1] && random.nextDouble() < 0.90f) {
+                population[i].mutate(random);
+                System.out.println("mutating " + i);
+            }
+        }
+
+
+        //Flip heuristic
+        for (int i = 0; i < population.length; i++) {
+            if (i != eliteIdx[0] && i != eliteIdx[1]) {
+                population[i].flipHeuristic(population[i].calcEvaluationValue(clauseList), clauseList, random);
+                System.out.println("Flipping " + i);
+            }
+        }
+
+        
+        // final eval values
+        evalSum = 0;
+        eval = new int[population.length];
+        for (int i = 0; i < population.length; i++) {
+            eval[i] = population[i].calcEvaluationValue(clauseList);
+            evalSum += eval[i];
+            System.out.println("" + i + ": " + eval[i]);
+        }
+
+        
+        System.out.println("time: " + (System.currentTimeMillis() - start));
+
+        return false;
+    }
 }
